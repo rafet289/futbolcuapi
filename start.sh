ccdce155aaa834c0a34b5240546cfbf775472b5d
rafet289 <79151705+rafet289@users.noreply.github.com>
2025-04-14 12:59:17 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-uvicorn main:app --host=0.0.0.0 --port=$PORT
+echo -e '#!/bin/bash\necho "Starting FastAPI app..."\nexec uvicorn main:app --host=0.0.0.0 --port=${PORT:-8000}' > start.sh
+chmod +x start.sh
